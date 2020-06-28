1b818f4fc1b1f118d3aae347e688ac823739751c
michael vargas loza <vargas_michael1993@yahoo.es>
2020-06-28 00:45:27 -0400

    Version 1
    Cargado de los archivos.

---
+++ Grid A Excel/Grid A Excel/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmInicio
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvTabla = New System.Windows.Forms.DataGridView()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnGenerar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        CType(Me.dgvTabla, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dgvTabla
+        '
+        Me.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvTabla.Location = New System.Drawing.Point(12, 31)
+        Me.dgvTabla.Name = "dgvTabla"
+        Me.dgvTabla.RowTemplate.Height = 24
+        Me.dgvTabla.Size = New System.Drawing.Size(758, 272)
+        Me.dgvTabla.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(782, 28)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SalirToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(71, 24)
+        Me.ArchivoToolStripMenuItem.Text = "&Archivo"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(152, 24)
+        Me.SalirToolStripMenuItem.Text = "&Salir"
+        '
+        'btnGenerar
+        '
+        Me.btnGenerar.Location = New System.Drawing.Point(564, 309)
+        Me.btnGenerar.Name = "btnGenerar"
+        Me.btnGenerar.Size = New System.Drawing.Size(100, 32)
+        Me.btnGenerar.TabIndex = 2
+        Me.btnGenerar.Text = "&Generar"
+        Me.btnGenerar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(670, 309)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(100, 32)
+        Me.btnGuardar.TabIndex = 3
+        Me.btnGuardar.Text = "&Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'frmInicio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(782, 353)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnGenerar)
+        Me.Controls.Add(Me.dgvTabla)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmInicio"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Grid A Excel"
+        CType(Me.dgvTabla, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dgvTabla As System.Windows.Forms.DataGridView
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents btnGenerar As System.Windows.Forms.Button
+    Friend WithEvents btnGuardar As System.Windows.Forms.Button
+
+End Class
---
+++ Grid A Excel/Grid A Excel/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Data.SqlClient
+
+Public Class frmInicio
+    Dim currentTime As Date
+
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
+        Try
+            Dim dt As New DataTable
+            Dim cn As String = "Server=;Database=;User Id=;Password=;Connection Timeout=8000"
+            Using adaptador As New SqlDataAdapter("query", cn)
+                adaptador.Fill(dt)
+            End Using
+            dgvTabla.DataSource = dt
+
+            MsgBox("Consulta generada correctamente.")
+        Catch ex As Exception
+            MsgBox("Error al consultar a la base de datos: " + ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Try
+            GridAExcel(dgvTabla)
+        Catch ex As Exception
+            MsgBox("Error al generar el archivo: " + ex.ToString)
+        End Try
+    End Sub
+
+    Function GridAExcel(ByVal ElGrid As DataGridView) As Boolean
+        currentTime = TimeOfDay
+
+        Dim correcto As Boolean = True
+        Dim d As Date
+        d = Date.Now
+        Dim completDate As String = d.ToString()
+        Dim aux() As String = completDate.Split(" ")
+        Dim fechaCompleta() As String = aux(0).Split("/")
+
+        Dim guardarTabla As New SaveFileDialog
+        'Destino donde se guarda el reporte
+        guardarTabla.FileName = "C:\DIRECCION\Reporte " & Now.Day & "-" & Now.Month & "-" & Now.Year & ".xlsx"
+
+        Try
+            ExportarExcel(dgvTabla, guardarTabla.FileName)
+        Catch
+            correcto = False
+        End Try
+        If correcto Then
+            'lbxEstado.Items.Add(currentTime + " Reporte [" + reporte + "] generado correctamente.")
+        End If
+        Return True
+    End Function
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Public Sub ExportarExcel(ByVal dgv As DataGridView, ByVal pth As String)
+        Dim xlApp As Object = CreateObject("Excel.Application")
+
+        'creamos una nueva hoja de calculo 
+        Dim xlWB As Object = xlApp.WorkBooks.add
+        Dim xlWS As Object = xlWB.WorkSheets(1)
+
+        'exportamos los caracteres de las columnas
+        For c As Integer = 0 To dgv.Columns.Count - 1
+            xlWS.cells(1, c + 1).value = dgv.Columns(c).HeaderText
+        Next
+
+        'exportamos contenido de las tablas
+        For r As Integer = 0 To dgv.RowCount - 1
+            For c As Integer = 0 To dgv.Columns.Count - 1
+                xlWS.cells(r + 2, c + 1).value = dgv.Item(c, r).Value
+            Next
+        Next
+
+        'guardamos la hoja de calculo en la ruta especificada 
+        xlWB.saveas(pth)
+        xlWS = Nothing
+        xlWB = Nothing
+        xlApp.quit()
+        xlApp.kill()
+        xlApp = Nothing
+    End Sub
+End Class
